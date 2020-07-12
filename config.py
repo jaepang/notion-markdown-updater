1b0800b3dc37d329950b1b207cd7bc36eb23bdd4
shinjawkwang <shinjawkwang@gmail.com>
2020-07-12 13:55:59 +0000

    init repo

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from notion.client import NotionClient
+
+# notion private information and init NotionClient
+NOTION_TOKEN = os.getenv("NOTION_TOKEN")
+HOME_URL = os.getenv("DOCUMENTS_URL")
+client = NotionClient(token_v2=NOTION_TOKEN, monitor=True, start_monitoring=True)
+blog_home = client.get_block(HOME_URL)
+COLLECTION_ID = blog_home.collection.id
+collection = client.get_collection(COLLECTION_ID)
+posts = collection.get_rows()
+
+# post status str
+publish_ready = "🚀Ready to Publish"
+published = "📰Published"
+dont_publish = "🚫Don\'t Publish"
+
+# the path where you want to place markdown file
+post_path = "../blog/"
---
+++ post_to_markdown.py
@@ -0,0 +1,100 @@
+import os
+import datetime
+
+from config import posts, post_path, publish_ready, published
+
+def parse_language(lan):
+    lan = lan.lower()
+    if lan == 'c++':
+        lan = 'cpp'
+    elif lan == 'c#':
+        lan = 'csharp'
+    elif lan == 'objective-c':
+        lan = 'objectivec'
+    return lan
+
+'''
+TODO
+    1. bookmark (link) not work
+'''
+def post_to_markdown(post, depth):
+    text = ""
+    if depth == 0:
+        # Handle Frontmatter
+        text += "---\ntitle: %s\ndate: %s\ndescription: %s\n---" % (post.title, post.created, post.description)
+        # Handle Title
+        text += "\n\n" + "# " + post.title + "\n\n"
+    numered_list_index = 1
+    bullet = False
+
+    for block in post.children:
+        for i in range(0, depth):
+            text += "\t"
+        # Handles Numbered List
+        if block.type == "numbered_list":
+            text += str(numered_list_index) + ". " + block.title + "\n"
+            numered_list_index += 1
+        else:
+            if numered_list_index > 1:
+                text += "\n"
+            numered_list_index = 1
+        # Handles Bullets List
+        if block.type == "bulleted_list":
+            text += "- " + block.title + "\n"
+            bullet = True
+        else:
+            if bullet:
+                text += "\n"
+            bullet = False
+        # Handles H1
+        if block.type == "header":
+            text += "# " + block.title + "\n\n"
+        # Handles H2
+        if block.type == "sub_header":
+            text += "## " + block.title + "\n\n"
+        # Handles H3
+        if block.type == "sub_sub_header":
+            text += "### " + block.title + "\n\n"
+        # Handles Code Blocks
+        if block.type == "code":
+            text += "``` " + parse_language(block.language) + "\n" + block.title + "\n```\n\n"
+        # Handles Images
+        if block.type == "image":
+            text += "![" + block.id + "](" + block.source + ")\n\n"
+        # Handles Dividers
+        if block.type == "divider":
+            text += "---" + "\n"
+        # Handles Basic Text, Links, Single Line Code
+        if block.type == "text":
+            text += block.title + "\n\n"
+        children = block.children
+        if bool(children):
+            text += post_to_markdown(block, depth+1)
+    return text
+
+def write_file(post, text):
+    title = post.title.replace(" ", "-")
+    title = title.replace(".", "")
+    title = title.replace(",", "")
+    title = title.replace(":", "")
+    title = title.replace(";", "")
+    title = title.lower()
+    try:
+        os.mkdir(post_path + title)
+        print("new directory created")
+    except:
+        pass
+    file = open(post_path + title + "/index.md", 'w')
+    print("Wrote A New Page")
+    file.write(text)
+
+def update_row(post):
+    text = post_to_markdown(post, 0)
+    write_file(post, text)
+
+# write file manually
+for post in posts:
+    if post.status == publish_ready:
+        text = post_to_markdown(post, 0)
+        write_file(post, text)
+        post.status = published
