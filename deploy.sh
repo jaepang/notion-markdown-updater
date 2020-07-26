6c5aaf8c899a0b34eb4d30842518b34334d8e59f
shinjawkwang <shinjawkwang@gmail.com>
2020-07-26 13:35:36 +0000

    :racehorse: deploy.sh now no need to add remote

--- deploy.sh
+++ deploy.sh
@@ -16,11 +16,7 @@ git init
 git add -A
 git commit -m 'deploy with vuepress'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/shinjawkwang/techArchive master:gh-pages
+git push -f git@github.com:jaepang/techArchive.git master:gh-pages
 
 
 cd -
